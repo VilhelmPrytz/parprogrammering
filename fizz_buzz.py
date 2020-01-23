b6beccadb882e974717f2b33d1a4f9951c705977
Ludwig Sjöberg <ludwig.sjoberg@skola.botkyrka.se>
2020-01-23 10:58:02 +0100

    fizzbuzz

---
+++ fizz_buzz.py
@@ -0,0 +1,19 @@
+def fizz_buzz(num):
+    if num % 3 == 0 and num % 5 == 0:
+        return "FizzBuzz"
+
+    if num % 3 == 0:
+        return "Fizz"
+    
+    if num % 5 == 0:
+        return "Buzz"
+    
+    return num
+
+
+print(fizz_buzz(3))
+print(fizz_buzz(5))
+print(fizz_buzz(15))
+print(fizz_buzz(4))
+
+
